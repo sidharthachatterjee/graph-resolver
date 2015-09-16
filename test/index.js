fb87a7fb4d737be3ab0549ed709ba32642f3fea0
Sidhartha Chatterjee <siddharthkchatterjee@gmail.com>
2015-09-16 17:00:27 +0530

    Clean up and add more tests

--- test/index.js
+++ test/index.js
@@ -1,13 +1,31 @@
+/*eslint-disable no-console */
+
+import _ from 'lodash';
 import chai from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
+chai.should();
 let expect = chai.expect;
+let assert = chai.assert;
 chai.use(sinonChai);
 
 import Resolver from '../lib';
 import {Graph} from 'graphlib';
 import {ensureArray, chainThenables} from '../lib/utilities';
-// import {setupGraph, getPath} from '../lib/graph';
+import {setupGraph} from '../lib/graph';
+
+const getItemsForEvent = input => input,
+  getEventsForBrand = input => input;
+
+const relationships = [{
+  from: 'Event',
+  to: 'Item',
+  method: getItemsForEvent
+}, {
+  from: 'Brand',
+  to: 'Event',
+  method: getEventsForBrand
+}];
 
 describe('Resolver', function () {
   it('has the resolve function in its prototype', function () {
@@ -15,18 +33,14 @@ describe('Resolver', function () {
     expect(Resolver.prototype.resolve).to.be.a('function');
   });
   it('constructs an object with an instance of Graph in its graph property', function () {
-    let relationships = [{
-      from: 'Event',
-      to: 'Item',
-      method: input => input + 1
-    }, {
-      from: 'Brand',
-      to: 'Event',
-      method: input => input + 1
-    }];
     let resolver = new Resolver(relationships);
     expect(resolver).to.have.property('graph');
     expect(resolver.graph).to.be.an.instanceof(Graph);
+  });
+  describe('resolve', function () {
+    it('works with a single value as a seed');
+    it('works with an array of values as a seed');
+    it('returns a Promise that resolves to a flat array of values (ObjectIds for now)');
   });
 });
 
@@ -87,5 +101,30 @@ describe('Utilities', function () {
         done();
       });
     });
+  });
+});
+
+describe('Graph', function () {
+  describe('setupGraph', function () {
+    it('returns a directed graph', function () {
+      let graph = setupGraph(relationships);
+      assert(graph.isDirected(), 'The graph is undirected');
+    });
+    it('returns a graph with all nodes in the relationships objects', function () {
+      let graph = setupGraph(relationships);
+      expect(graph.nodeCount()).to.equal(3);
+      expect(graph.nodes()).to.include.members(['Event', 'Item', 'Brand']);
+    });
+    it('returns a graph with all edges in the relationships objects', function () {
+      let graph = setupGraph(relationships);
+      expect(graph.edgeCount()).to.equal(2);
+      expect(graph.edges().map(_.ary(graph.edge, 1), graph)).to.include.members([getEventsForBrand, getItemsForEvent]);
+    });
+  });
+  describe('getPath', function () {
+    it('pushes the right edge into the path array');
+    it('returns when source and predecessor are equal');
+    it('returns the path in the correct order');
+    it('calls itself recursively if source and predecessor are not equal');
   });
 });
